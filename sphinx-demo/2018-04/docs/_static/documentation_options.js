2f65fcc6b155a3370927fed9c9834970c7bebcc8
lucdanton <lucdanton@free.fr>
2018-04-13 11:11:32 +0200

    Refresh docs.

--- sphinx-demo/2018-04/docs/_static/documentation_options.js
+++ sphinx-demo/2018-04/docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '1.8.0+/156bb0aa3',
+    VERSION: '1.8.0+/3d40b7278',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
